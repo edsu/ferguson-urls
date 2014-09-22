850e65317ede3c33e205163da1c8d14595507a67
Ed Summers <ehs@pobox.com>
2014-09-22 13:44:47 +0000

    initial

---
+++ README.md
@@ -0,0 +1,23 @@
+# ferguson-urls 
+
+This report and underlying data represent 417,972 unique URLs that were in 
+13,480,000 tweets that mentioned "ferguson" between August 10th and August
+27th, 2014. You can learn more about how the data was collected 
+[here](http://inkdroid.org/journal/2014/08/30/a-ferguson-twitter-archive/).
+
+The URLs themselves were extracted from the collected Twitter data, unshortened
+(using [unshrtn](http://github.com/edsu/unshrtn) and then loaded into 
+[redis](http://redis.io) in order to count them.
+
+The tweets.tsv.gz here is a tab separated file that includes the tweeted 
+URL followed by the tweet URLs for any tweets that mentioned it.
+
+The index.html file is a report of the top 100 URLs tweeted, which indicates
+whether the URL appears to have been archived by the Internet Archive. It uses
+the data found in urls.json, which is generated with urls.py.
+
+## License
+
+# Code and data are licensed with [CC-SA](https://creativecommons.org/licenses/by-sa/2.0/)
+
+
---
+++ index.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import bs4
+import gzip
+import json
+import requests
+
+LIMIT = 100
+
+def archived(url):
+    ia = "http://web.archive.org/cdx/search/cdx"
+    params = {"output": "json", "url": url}
+    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.122 Safari/537.36'}
+    try:
+        results = requests.get(ia, params=params, headers=headers)
+        return len(results.json()) > 0
+    except:
+        return False
+
+def title(url):
+    try:
+        resp = requests.get(url)
+        if 'text/html' in resp.headers['content-type']:
+            html = resp.content
+            soup = bs4.BeautifulSoup(html)
+            return soup.title.text
+        else:
+            return None
+    except:
+        return None
+
+urls = []
+for line in gzip.open("urls.tsv.gz"):
+    cols = line.split("\t")
+    r = {
+        "url": cols[0],
+        "tweet_sample": cols[1:10],
+        "tweet_count": len(cols[1:]),
+        "archived": archived(cols[0]),
+        "title": title(cols[0])
+    }
+    urls.insert(0, r)
+    if len(urls) > LIMIT:
+        break
+
+open('urls.json', 'w').write(json.dumps(urls, indent=2))
